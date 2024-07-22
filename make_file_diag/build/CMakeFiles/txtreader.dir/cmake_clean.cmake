c9a9f0f804585155e969aa77fba2b3861cc24973
YeShaohua <yeshaohua@LAPTOP-58HBBHIF.>
2024-07-22 20:40:33 +0800

    diag

---
+++ make_file_diag/build/CMakeFiles/txtreader.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/txtreader.dir/txtread.cpp.o"
+  "CMakeFiles/txtreader.dir/txtread.cpp.o.d"
+  "txtreader"
+  "txtreader.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/txtreader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
